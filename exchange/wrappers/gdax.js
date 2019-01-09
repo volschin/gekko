545064383a107b184c84d563b095143dddd916ca
Jack Crypto49er <39413731+crypto49er@users.noreply.github.com>
2019-01-09 15:16:38 -0500

    Added 409 to recoverable errors in gdax wrapper

--- exchange/wrappers/gdax.js
+++ exchange/wrappers/gdax.js
@@ -59,6 +59,7 @@ const recoverableErrors = [
   'Response code 5',
   'GDAX is currently under maintenance.',
   'HTTP 408 Error',
+  'HTTP 409 Error',
   'HTTP 504 Error',
   'HTTP 503 Error',
   'socket hang up',
