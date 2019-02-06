94768d38cbb3f7b8b1a3f65dcd9d35659b5309d2
crypto49er <crypto49er@protonmail.com>
2019-02-06 17:32:49 -0500

    Set Gdax to use sandbox

--- exchange/wrappers/gdax.js
+++ exchange/wrappers/gdax.js
@@ -11,8 +11,8 @@ const QUERY_DELAY = 350;
 const marketData = require('./coinbase-markets.json');
 
 const Trader = function(config) {
-  this.post_only = true;
-  this.use_sandbox = false;
+  this.post_only = false;
+  this.use_sandbox = true;
   this.name = 'GDAX';
   this.scanback = false;
   this.scanbackTid = 0;
