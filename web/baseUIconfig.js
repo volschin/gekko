17f7bd5ec3b516deceeba365db29c68674f033d4
agent48co <agent48co@yandex.ru>
2019-06-16 14:06:45 +0300

    initial set-up (pg, neuralnet, etc)

--- web/baseUIconfig.js
+++ web/baseUIconfig.js
@@ -8,16 +8,16 @@ const CONFIG = {
   headless: false,
   api: {
     host: '127.0.0.1',
-    port: 3000,
+    port: 4000,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
     host: 'localhost',
-    port: 3000,
+    port: 4000,
     path: '/'
   },
-  adapter: 'sqlite'
+  adapter: 'postgresql'
 }
 
 if(typeof window === 'undefined')
--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -57,8 +57,8 @@ config.sqlite = {
 config.postgresql = {
   path: 'plugins/postgresql',
   version: 0.1,
-  connectionString: 'postgres://user:pass@localhost:5432', // if default port
-  database: null, // if set, we'll put all tables into a single database.
+  connectionString: 'postgres://postgres:Slon48@localhost:5432', // if default port
+  database: 'gekko', // if set, we'll put all tables into a single database.
   schema: 'public',
   dependencies: [{
     module: 'pg',
@@ -81,7 +81,9 @@ config.adviceWriter = {
   enabled: false,
   muteSoft: true,
 }
-
+config.adviceLogger = {
+  enabled: true
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING BACKTESTING
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
