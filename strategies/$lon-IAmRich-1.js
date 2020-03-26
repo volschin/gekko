fcab8a685d3bdafc0b4324a06c7d79e159aa1fd7
involve-it <arutune@gmail.com>
2020-03-26 15:33:08 +0300

    formatted for async base

--- strategies/$lon-IAmRich-1.js
+++ strategies/$lon-IAmRich-1.js
@@ -1,11 +1,5 @@
 // IAmRich strategy
 // v-1
-const moment = require('moment');
-let _ = require('lodash');
-let log = require('../core/log.js');
-
-let bb = require('./indicators/BB.js');
-let rsi = require('./indicators/RSI.js');
 
 let strat = {};
 const CandleBatcher = require('../core/candleBatcher');
@@ -17,31 +11,16 @@ let MA = SMA;
 
 strat.init = function() {
 
-  const config = require ('../core/util').getConfig() || {};
-  if (config.tradingAdvisor.candleSize === 15) {
-    log.remote('This strategy must run with candleSize=15');
-    // throw "This strategy must run with candleSize=15";
-  }
-  let currentCandle, currentPrice = 0.0, buyPrice = 0.0, advised = false, tradeInitiated = false, buyTs, candlesArr = Array(10).fill({
-      open: -1,
-      close: -1,
-      high: -1,
-      low: -1
-    }),
-    aaatLengh;
-
-  consoleLog(`strat init, gekkoId: ${ config.gekkoId }, type: ${ config.type }`)
-
-  // debug? set to false to disable all logging/messages/stats (improves performance in backtests)
-  this.debug = false;
-
-  // performance
-  config.backtest.batchSize = 1000; // increase performance
-  config.silent = false;
-  config.debug = true;
+  let aaatLengh, aaatTrendUp, aaatTrendUpPrev, aaatStop, bb, rsi, rsiVal, longCandle, isMarketLostForThisTrend = false;
+  let totalUptrends = 0, totalDntrends = 0, totalBought = 0, totalSold = 0, totalBoughtAttempts = 0, totalSoldAttempts = 0,
+    totalTradesSuccess = 0, totalTradesLongCandleBelowAaat = 0, totalTradesAaatStopLoss = 0, totalHighVolumeCandles = 0;
+  let greenPrev = 0, redPrev = 0;
 
   this.name = '$lon-IAmRich-1';
-
+  // debug? set to false to disable all logging/messages/stats (improves performance in backtests)
+  this.config.silent = false;
+  this.config.debug = true;
+  this.debug = true;
   this.requiredHistory = this.tradingAdvisor.historySize;
 
   // define the indicators we need
@@ -51,7 +30,6 @@ strat.init = function() {
   let shortMA15 = new MA(21);
   let middleMA15 = new MA(100);
   let longMA15 = new MA(200);
-
   let shortMA60 = new MA(21);
   let middleMA60 = new MA(100);
   let longMA60 = new MA(200);
@@ -68,7 +46,7 @@ strat.init = function() {
 
   this.update60 = function(candle) {
     if(this.debug) {
-      consoleLog(`strat updateAaat:: candle: ${JSON.stringify(candle)}`);
+      this.consoleLog(`strat updateAaat:: candle: ${JSON.stringify(candle)}`);
     }
     aaatInd.update(candle);
 
@@ -80,14 +58,6 @@ strat.init = function() {
   batcherAaat.on('candle', this.update60);
 
   this.update = function(candle) {
-    if(this.debug) {
-      // consoleLog(`strat update:: advised: ${ advised }, tradeInitiated: ${ tradeInitiated }`);
-    }
-
-    currentPrice = candle.close;
-    currentCandle = candle;
-    candlesArr.pop();
-    candlesArr.unshift(candle);
 
     batcherAaat.write([candle]);
     batcherAaat.flush();
@@ -103,13 +73,11 @@ strat.init = function() {
     longMA15.update(candle.close);
   }
 
-  let aaatTrendUp, aaatTrendUpPrev, aaatStop, bb, rsi, rsiVal;
-
   this.check = function(candle) {
-    let candlePrev = candlesArr[1];
+    let candlePrev = this.candlesArr[1];
     if(this.debug) {
       // consoleLog(`strat check:: price: ${ price }, aaat: ${ JSON.stringify(aaat) }`);
-      consoleLog(`strat check:: candle.close: ${ candle.close }, candle.volume: ${ candle.volume }, ma.short: ${ shortMA60.result }, ma.middle: ${ middleMA60.result }, ma.long: ${ longMA60.result }, rsiVal: ${ rsiVal }, aaatStop: ${ aaatStop }, aaatTrendUp: ${ aaatTrendUp }, bb.lower: ${ bb.lower }, bb.upper: ${ bb.upper }, bb.middle: ${ bb.middle }, advised: ${ advised }, tradeInitiated: ${ tradeInitiated }`);
+      this.consoleLog(`strat check:: candle.close: ${ candle.close }, candle.volume: ${ candle.volume }, ma.short: ${ shortMA60.result }, ma.middle: ${ middleMA60.result }, ma.long: ${ longMA60.result }, rsiVal: ${ rsiVal }, aaatStop: ${ aaatStop }, aaatTrendUp: ${ aaatTrendUp }, bb.lower: ${ bb.lower }, bb.upper: ${ bb.upper }, bb.middle: ${ bb.middle }`);
     }
 
     if(aaatTrendUp) {
@@ -125,7 +93,7 @@ strat.init = function() {
         candlePrev.isHighVolumeDown = true;
       }
 
-      if(!advised) {
+      if(!this.advised) {
         if (candle.close <= bb.lower && rsiVal <= this.settings.rsi.low && candle.close > aaatStop) {
           //this.buy('bb-rsi trending up');
         }
@@ -145,7 +113,7 @@ strat.init = function() {
           // this.buy('bb.lower crossed');
         }
         // volume  based (buy after volume up / sell after volume down):
-        if(candlePrev.isHighVolumeDown  && !candlesArr[2].isHighVolumeUp && candle.close > aaatStop) {
+        if(candlePrev.isHighVolumeDown  && !this.candlesArr[2].isHighVolumeUp && candle.close > aaatStop) {
 
         }
         // this.buy('candlePrev.isHighVolumeDown');
@@ -161,9 +129,9 @@ strat.init = function() {
         } else if(candle.close > bb.middle && rsiVal >= this.settings.rsi.high) {
           //this.sell('bb-rsi trending down');
         }
-        if (candle.close >= buyPrice * this.settings.takeProfit) {
-          console.error(`${ buyPrice }, ${ candle.close }`)
-          this.sell(`SELL!!: TAKE PROFIT, buy: ${ buyPrice }, sell: ${ candle.close }`);
+        if (candle.close >= this.buyPrice * this.settings.takeProfit) {
+          console.error(`${ this.buyPrice }, ${ candle.close }`)
+          this.sell(`SELL!!: TAKE PROFIT, buy: ${ this.buyPrice }, sell: ${ candle.close }`);
           totalTradesSuccess++;
         }
         if(candlePrev.isHighVolumeUp) {
@@ -182,8 +150,8 @@ strat.init = function() {
     } else {
 
       hadTrade = false;
-      if(advised) {
-        if(candle.close <= buyPrice * 0.95) {
+      if(this.advised) {
+        if(candle.close <= this.buyPrice * 0.95) {
           // this.sell('stop loss, trending dn, stop loss 10%');
         }
         // this.sell('stop loss, trending dn!');
@@ -192,98 +160,22 @@ strat.init = function() {
     aaatTrendUpPrev = aaatTrendUp;
   }
 
-  this.sell = function(reason) {
-    consoleLog(`strat.sell:: attempting, tradeInitiated=${ tradeInitiated }`);
-    totalSoldAttempts ++;
-    if (!tradeInitiated) { // Add logic to use other indicators
-      consoleLog(`strat.sell:: called, reason: ${ reason }`);
-      this.notify({
-        type: 'sell advice',
-        reason: reason,
-      });
-      this.advice('short');
-      advised = false;
-      if(currentPrice > buyPrice * 1.002) {
-        totalTradesSuccess++;
-      }
-      buyPrice = 0;
-      totalSold ++;
-    }
-  }
-
-  this.buy = function(reason) {
-    consoleLog(`strat.buy:: attempting, tradeInitiated=${ tradeInitiated }`);
-    totalBoughtAttempts++;
-    advised = true;
-    // If there are no active trades, send signal
-    if (!tradeInitiated) { // Add logic to use other indicators
-      consoleLog(`strat.buy:: called, reason: ${ reason }`);
-      this.notify({
-        type: 'buy advice',
-        reason: reason,
-      });
-      this.advice('long');
-      buyTs = this.candle.start;
-      buyPrice = currentPrice;
-      tradeInitiated = true;
-      totalBought++;
-    }
-  }
-  this.onPendingTrade = function(pendingTrade = {}) {
-    tradeInitiated = true;
-    consoleLog('onPendingTrade (tradeInitiated = true)');
-  }
-
-  this.onTrade = function(trade = {}) {
-    tradeInitiated = false;
-    buyPrice = trade.price;
-    consoleLog(`onTrade:: trade: ${ JSON.stringify(trade) }`);
-  }
-
-  // Trades tht didn't complete with a buy/sell (see processTradeErrored in tradingAdvisor)
-  this.onTerminatedTrades = function(terminatedTrades = {}) {
-    tradeInitiated = false;
-    consoleLog('onTerminatedTrades:: Trade failed. Reason: ' + terminatedTrades.reason);
-  }
-
-  this.onPortfolioChange = function(portfolio) {
-    consoleLog(`onPortfolioChange, portfolio: ${ JSON.stringify(portfolio) }`);
-  }
-  this.onPortfolioValueChange = function(portfolio) {
-    // consoleLog(`onPortfolioValueChange, portfolio: ${ JSON.stringify(portfolio) }`);
-  }
-  this.onTriggerFired = function(data) {
-    consoleLog(`onTriggerFired, caused by trailing stop, data: ${ JSON.stringify(data) }`);
-  }
-
-  let totalUptrends = 0, totalBought = 0, totalSold = 0, totalBoughtAttempts = 0, totalSoldAttempts = 0,
-    totalTradesSuccess = 0, totalTradesAaatStopLoss = 0, totalHighVolumeCandles = 0;
   this.end = function(a, b, c) {
-    consoleLog('gekko end')
-    console.error('gekko end, here is some statistics for you Sir:')
+    console.error('Here is some statistics for you Sir:')
     console.error(`totalUptrends: ${ totalUptrends }, totalBought: ${ totalBought } (out of ${ totalBoughtAttempts } attempts), totalSold: ${ totalSold
       } (out of ${ totalSoldAttempts } attempts), statsTotalTradesSuccess: ${ totalTradesSuccess }, totalTradesAaatStopLoss: ${ totalTradesAaatStopLoss
     }, totalHighVolumeCandles: ${ totalHighVolumeCandles }`);
 
   }
-  function consoleLog(msg){
-    if(config){
-      msg = msg || '';
-      currentCandle = currentCandle || {}
-      const prefix = `${ config.gekkoId }, ${ JSON.stringify(currentCandle.start) || JSON.stringify(moment()) } -- `;
-      console.log(prefix, msg);
-      log.debug(prefix, msg);
-    }
-  }
+
   const printCandle = function(candle) {
-    consoleLog(`strat check:: candle.close: ${ candle.close }, candle.volume: ${ candle.volume }, rsiVal: ${ rsiVal }, aaatStop: ${ aaatStop }, aaatTrendUp: ${ aaatTrendUp }, bb.lower: ${ bb.lower }, bb.upper: ${ bb.upper }, bb.middle: ${ bb.middle }, advised: ${ advised }, tradeInitiated: ${ tradeInitiated }`);
+    this.consoleLog(`strat check:: candle.close: ${ candle.close }, candle.volume: ${ candle.volume }, rsiVal: ${ rsiVal }, aaatStop: ${ aaatStop }, aaatTrendUp: ${ aaatTrendUp }, bb.lower: ${ bb.lower }, bb.upper: ${ bb.upper }, bb.middle: ${ bb.middle }, advised: ${ this.advised }, tradeInitiated: ${ this.tradeInitiated }`);
   }
 }
 
 strat.check = function(){
   // gekko stub
 }
-
 module.exports = strat;
 
 
